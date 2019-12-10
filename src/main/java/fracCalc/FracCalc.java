3156a45f5ef28e2b0ded6f3c7816ac3a42cae35b
s-huangi <s-huangi@53S50025634.it.bsd405.org>
2019-12-09 17:34:17 -0800

    final frac calc

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -24,70 +24,70 @@ public class FracCalc {
     	
     	
  }   	
-    	    	
-        // TODO: Read the input from the user and call produceAnswer with an equation
-//still needs to run-user sentinel loop is going to run here//take input and parse it
-    	//call produceAnswer
-    	//system.out.println statement
-    
-
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-     
+    	    	  
      
     public static String produceAnswer(String input)
     {//does all of the work
         // TODO: Implement this function to produce the solution to the input
     	
-    	/*operand
-    	operator
-    	
-    	second operand*/
+
     
-    	//operand
+		/*// simple operation starts //////
+		 * input = input.trim();
+		 * 
+		 * String frac = input; String operand1 = frac.substring(0, frac.indexOf(' '));
+		 * frac = frac.substring(frac.indexOf(' ') + 1); String operator =
+		 * frac.substring(0, frac.indexOf(' ')); frac = frac.substring(frac.indexOf(' ')
+		 * + 1); String operand2 = frac;
+		 * 
+		 * String chk3Answer = "";
+		 * 
+		 * chk3Answer = evaluate(operator, operand1, operand2);
+		 */////simple operation end //////
     	
+    	//////////////////////////////// Multiple Operations Start/////////////////// 
+        // it's fine for operations contain one or more spaces, example "   5_3/4   -   -6_8/8   -  5_3/4 "
+        // store input in a string array
+    	//extra credit
+    	String[] fracStringArray = input.split("\\s+"); // The input string split into substrings by one or more spaces
+
+    	if(fracStringArray.length<3) { // the arithmetic operation should have at least 3 elements, like 1 + 2, or 1 + 2 + 3
+    		return "ERROR: Input is in an invalid format.";
+    	}
     	
-    	/*Scanner s = new Scanner(input);
-    	String operand1 = s.next();
-    	String operator = s.next();
-    	String operand2 = s.next();
+    	// invalid: 1 + 
+    	// invalid: 1 + 2 +
+    	// even number of operand and operator is invalid
+    	if((fracStringArray.length % 2)==0) 
+    		return "ERROR: Input is in an invalid format.";   
+
+    	// simple operation 1 + 2 => string array ["1","+","2"], operand1 is "1", operator is "+", operand2 is "2"
+    	// chk3Answer = evaluate("+", "1", "2"); so chk3Answer = 3
     	
-    	
-    	return operand2;*/
-    	input = input.trim();
-    	
-    	String frac = input;
-    	String operand1 = frac.substring(0, frac.indexOf(' '));
-    	frac = frac.substring(frac.indexOf(' ') + 1);
-    	String operator = frac.substring(0, frac.indexOf(' '));
-    	frac = frac.substring(frac.indexOf(' ') + 1);
-    	String operand2 = frac;
-    	
-    	
-     
-    	/*String op2Whole = findWhole(operand2);
-    	String op2Num = findNum(operand2);
-    	String op2Denom = findDenom(operand2);
-    	String chk2Answer = "whole:" + op2Whole + " numerator:" 
-    	+ op2Num + " denominator:" + op2Denom;*/
     	String chk3Answer = "";
+    	if(fracStringArray.length >= 3) {
+    		String operand1 = fracStringArray[0]; // index 0 is the the 1st element in the array which is "1"
+    		String operator = fracStringArray[1]; // index 1 is the the 2nd element in the array which is "+"
+    		String operand2 = fracStringArray[2]; // index 2 is the the 3th element in the array which is "2"
+    		chk3Answer = evaluate(operator, operand1, operand2);
+    	}
     	
-    	chk3Answer = evaluate(operator, operand1, operand2);
+    	// multiple operations example 1 + 2 + 3 + 4
+    	// from "1 + 2 " the above code section we got chk3Answer = 3
     	
+    	// Next, first loop for " + 3 " do chk3Answer = evaluate("+", "chk3Answer", "3"), chk3Answer will be 3 + 3 = 6
+    	// then second loop for " + 4 " do chk3Answer = evaluate("+", "chk3Answer", "4") chk3Answer will be 6 + 4 = 10
+        for (int i=3; i < fracStringArray.length; i+=2) {
+    		String operator = fracStringArray[i]; // index i is the the first element in the array which is "+"
+    		String operand = fracStringArray[i+1]; // index i+1 is the the first element in the array which is "3"
+    		chk3Answer = evaluate(operator, chk3Answer, operand);
+    	}
+        ////////////////////////////////Multiple Operations End/////////////////// 
         return chk3Answer; 
     }
     
     public static String findWhole(String str) {
-    	
-    	//mixed fraction 3_4/5
-    	
-    	
+    	  	
     	if(str.indexOf('_') != -1) {
     		return str.substring(0, str.indexOf('_'));
     	}else if (str.indexOf('/') != -1) {
@@ -117,21 +117,11 @@ public class FracCalc {
     
     public static String evaluate(String operator, String operand1, String operand2) {
     	String chk3Answer = "";
-    		/*if(operator.equals("+")) {
-    			chk3Answer = add(operand1, operand2);
-    		}else if(operator.equals("-")) {
-    			chk3Answer = subtract(operand1, operand2);
-    		}else if(operator.equals("*")) {
-    			chk3Answer = multiply(operand1, operand2);
-    		}else if(operator.equals("/")) {
-    			chk3Answer = divide(operand1, operand2);
-    		}else {
-    			return "ERROR: Input is in an invalid format.";//extra credit?
-    		}*/
+    	
     	chk3Answer = operation (operator, operand1, operand2);
     	return chk3Answer;
     	}
-    	
+  //math operations  	
   public static String operation(String operator, String operand1, String operand2) {
         
         // find whole,num and denom from operand1 and operand2
@@ -154,6 +144,7 @@ public class FracCalc {
             }
         
         // convert string to integer
+        //parse fraction
         int  whole1 = Integer.parseInt(op1Whole);
         int  num1 = Integer.parseInt(op1Num);
         int  denon1 = Integer.parseInt(op1Denom);
@@ -174,60 +165,54 @@ public class FracCalc {
         }
          // check preconditions
          if ((denon1 == 0) || (denon2 == 0)) {
-             return"ERROR: Cannot divide by zero.";
+             return"ERROR: Cannot divide by zero."; 
+             //cannot divide or multiply by 0
          }
-        int denominator = 1;// default set to 1, like 0/1
+        int denominator = 1;    // default set to 1, like 0/1
         int numerator = 0;     //default set to 0;
         int whole = 0;        //default set to 0;
 
-        // operation calculation        
+        // operation calculation  
+        //addition
         if(operator.equals("+")) {
             // example 1_2/3 + -4_5/6, denominator = 3 * 6 = 18
             denominator = denon1 * denon2; // 18            
             // numerator = (1 * denominator) + (2 * 6) * (1) + (-4)*denominator + (5*3) * (-1)
-//            numerator = (whole1 * denominator) + (num1 * denon2) * signed1 + 
-//                (whole2 * denominator) + (num2 * denon1) * signed2;
             numerator = (whole1 * denon1 + num1 ) * denon2 * signed1 +
                 (whole2 * denon2 + num2) * denon1 * signed2;
-            
+        
+        //subtract
         }else if(operator.equals("-")){
-            // comments are similar to the add function
+            // similar to the add function
             denominator = denon1 * denon2;            
             numerator = (whole1 * denon1 + num1 ) * denon2 * signed1 -
                 (whole2 * denon2 + num2) * denon1 * signed2;
-            
+        
+        //multiply
         }else if(operator.equals("*")){
             // example 2_3/4 * 5_6/7 = (2*4 + 3)/4 * (5*7 + 6)/7 
             // whole1 = 2, num1 = 3, denon1 = 4; whole2 = 5, num2 = 6, denon2 = 7; 
             // numerator = (2*4 + 3)*(5*7 + 6), denominator = 4*7
-            // numerator = (whole1*denon1 + num1)*(whole2*denon2 + num2)  
-            // denominator = denon1*denon2
             denominator = denon1 * denon2;            
             numerator = (whole1 * denon1 + num1 ) * signed1 *
                 (whole2 * denon2 + num2) * signed2;
     
         }
+        //divide
         else if(operator.equals("/")){
             // example 2_3/4 / 5_6/7 = (2*4 + 3)/4 / (5*7 + 6)/7 
             // whole1 = 2, num1 = 3, denon1 = 4; whole2 = 5, num2 = 6, denon2 = 7; 
             // numerator = (2*4 + 3)*7, denominator = 4*(5*7 + 6)
             denominator = denon1 * ((whole2*denon2 + num2) * signed2);            
             numerator = denon2 * ((whole1*denon1 + num1) * signed1 );
+                  
         }else {
             return "ERROR: Input is in an invalid format.";
         }      
         
-/*        // example 2_3/4 * 5_6/7 = (2*4 + 3)/4 * (5*7 + 6)/7 
-        // whole1 = 2, num1 = 3, denon1 = 4; whole2 = 5, num2 = 6, denon2 = 7; 
-        // numerator = (2*4 + 3)*(5*7 + 6), denominator = 4*7
-        // numerator = (whole1*denon1 + num1)*(whole2*denon2 + num2)  
-        // denominator = denon1*denon2
-        int denominator = denon1 * denon2;
-        
-        int numerator = ((whole1 * denon1) + num1 * signed1) *
-            ((whole2 * denon2) + num2 * signed2);
+        // example 2_3/4 * 5_6/7 = (2*4 + 3)/4 * (5*7 + 6)/7 
+        // whole1 = 2, num1 = 3, denon1 = 4; whole2 = 5, num2 = 6, denon2 = 7;    
     
-    */
         whole = numerator/denominator;
         numerator = numerator%denominator;
         
@@ -238,8 +223,13 @@ public class FracCalc {
            numerator = numerator * (-1);       // then multiple -1 to make numerator positive
         }
         
-        if (whole != 0 && denominator < 0) {     // if whole is not 0, and denom < 0 which is negative 
-            denominator = denominator * (-1);       // then multiple -1 to make denom positive
+        if (whole != 0 && denominator < 0) {     // if whole is not 0, and denominator < 0 which is negative 
+            denominator = denominator * (-1);       // then multiple -1 to make denominator positive
+         }
+        
+        if (whole == 0 && denominator < 0) {     // if whole is 0, and denominator < 0 ( negative ), put the negative sign in numerator
+            denominator = denominator * (-1);    // by multiple -1 to make denominator positive
+            numerator = numerator * (-1);        // and multiple -1 to make numerator negative
          }
         
         String wholeStr = "";
@@ -260,7 +250,8 @@ public class FracCalc {
         return wholeStr + "_" + fractionStr;
     }
     
-    //greatest common factor
+  
+    //greatest common factor of 2 inputs
     public static int gcd(int denom1, int denom2)
     {
        int factor = denom2;
@@ -274,10 +265,12 @@ public class FracCalc {
     	
     public static String reduce(int numerator, int denominator)
     {
+    	//improper fraction-->mixed fraction
+    	
        int common = 0;
        // get absolute values for numerator and denominator
        int num = Math.abs(numerator);
-       int den = Math.abs(denominator);
+       int den = Math.abs(denominator);//cannot be 0
        // figure out which is less, numerator or denominator
        if (num > den)
           common = gcd(num, den);
@@ -289,13 +282,13 @@ public class FracCalc {
        numerator = numerator / common;
        denominator = denominator / common;
        
-       if(numerator == 0) {
+       if(numerator == 0) { 
      	  return "";
        }
        return Integer.toString(numerator) + "/" + Integer.toString(denominator);
     }
     
-    // check a string is a number
+    // check if string is a number
    
     public static boolean isNumeric(String str)
     {
@@ -309,14 +302,5 @@ public class FracCalc {
         return true;
     }
  }
-
-
-
-    	//fraction	
-    	//whole number
-    	
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
-
 
 
